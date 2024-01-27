4ff8bbade5600630dee0034999a5d243fe9387a2
a-wotton <a_wotton@fanshaweonline.ca>
2024-01-27 12:49:13 -0500

    styled list, fixed error where only second page gets fetched and added loading symbol.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 (() => {
 const baseUrl = `https://swapi.dev/api/`;
 const characters = [];
-const characterList = document.querySelector("#character-con")
+const characterList = document.querySelector("#character-con");
+const spinner = document.querySelector("#spinner");
 
 function getpage1() {
     fetch(`${baseUrl}people/`)
@@ -10,25 +11,26 @@ function getpage1() {
        //console.log(page1);
        const page1 = response.results;
        characters.push(page1[0], page1[1], page1[2], page1[3], page1[4], page1[9]);
+       getpage2();
     })
     .catch(error => {
         console.log(error)
         //send message to user in DOM
     });
 }
-getpage1();
-
-function getpage2() {
+    getpage1();
+    
+    function getpage2() {
     fetch(`${baseUrl}people/?page=2`)
     .then(response => response.json())
     .then(function(response) {
        const page2 = response.results;
-      // console.log(page2);
+       console.log(page2);
        characters.push(page2[0], page2[2], page2[3], page2[8]);
 
 const ul = document.createElement("ul");
 
-console.log(characters)
+console.log(characters);
 characters.forEach(character => {
 console.log(character);
 const li = document.createElement("li");
@@ -36,6 +38,7 @@ const a = document.createElement("a");
 a.textContent = character.name;
 li.appendChild(a);
 ul.appendChild(li);
+spinner.classList.add("hidden");
 })
 
 characterList.appendChild(ul);
@@ -46,5 +49,4 @@ characterList.appendChild(ul);
         //send message to user in DOM
     });
 }
-getpage2();
 })();
